11da9748397c88a8ddfbc50e210c72c59ad05666
Ramiz Polic <ramiz.polic@hotmail.com>
2024-02-01 15:30:15 +0100

    get required keys

--- cmd/regparser.go
+++ cmd/regparser.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	registryFilePath = "/Users/rpolic/Desktop/repos/regparser/testdata/NTUSER.DAT"
+	registryFilePath = "/home/ramiz-polic/Documents/win10/SOFTWARE"
 )
 
 func main() {
@@ -25,34 +25,35 @@ func main() {
 		os.Exit(1)
 	}
 
-	// recursively list all keys
-	recurseListMany(registry, []string{
-		"Software/Microsoft/Windows NT/CurrentVersion",
+	// list all keys from these paths
+	listPaths(registry, []string{
+		//"Microsoft/Windows NT/CurrentVersion",                                 // platform, needs filter
+		"Microsoft/Windows/CurrentVersion/Component Based Servicing/Packages", // installed kbs, needs filter and comparator
+		//"Microsoft/Windows NT/CurrentVersion/ProfileList",                     // user profiles, needed for per-user reader
+		//"Microsoft/Windows/CurrentVersion/Uninstall",                          // installed apps
+		//"Wow6432Node/Microsoft/Windows/CurrentVersion/Uninstall",              // installed apps
 	})
 }
 
-func recursiveList(registry *regparser.Registry, keyName string) {
-	key := registry.OpenKey(keyName)
+func listKeysFromPath(registry *regparser.Registry, keyPath string) {
+	key := registry.OpenKey(keyPath)
 	if key == nil {
-		fmt.Printf("---- %s NOT FOUND!", keyName)
 		return
 	}
 
-	fmt.Printf("\n ==== %s Subkeys:\n\n", keyName)
 	for _, subkey := range key.Subkeys() {
-		fmt.Printf(" %s - %v\n", subkey.Name(), subkey.LastWriteTime())
-
-		recursiveList(registry, path.Join(keyName, subkey.Name()))
+		fmt.Printf("%-120s\n", path.Join(keyPath, subkey.Name()))
 	}
 
-	fmt.Printf("\n ==== %s Values:\n\n", keyName)
+	// print all keys under this path
 	for _, value := range key.Values() {
-		fmt.Printf(" %s : %#v\n", value.ValueName(), value.ValueData())
+		// THIS CAN BE UNICODE/UTF-16 VALUE!!!
+		fmt.Printf("%-120s : %#v\n", path.Join(keyPath, value.ValueName()), value.ValueData().String)
 	}
 }
 
-func recurseListMany(registry *regparser.Registry, keys []string) {
+func listPaths(registry *regparser.Registry, keys []string) {
 	for _, key := range keys {
-		recursiveList(registry, key)
+		listKeysFromPath(registry, key)
 	}
 }
